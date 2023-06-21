2cd95ebb420e9652c34ddb633754c66f9c3e29f3
Leo <leodarkseid@gmail.com>
2023-06-21 21:54:30 +0100

    portfolio update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -18,10 +17,6 @@ const Header = () => {
         
 
         <HeaderSocials />
-
-        {/* <div className="me">
-          <img src={ME} alt="me" />
-        </div> */}
 
         <a href="contact" className='scroll__down'>Scroll Down</a>
       </div>
